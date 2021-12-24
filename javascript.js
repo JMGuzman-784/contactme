d346761bd637e8ab11a23365452589aaf8879e1a
Jose Guzman <Josemguzman784@gmail.com>
2021-12-24 00:11:44 -0600

    mmm

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ span1.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal1) {
     modal1.style.display = "none";
   }
 }
